6366d36ba62551f166396035b217dc1a098b3467
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-07-05 14:08:07 +0530

    Update

--- src/pygma/platform_detection.py
+++ src/pygma/platform_detection.py
@@ -4,7 +4,7 @@ import os
 
 def get_current_platform() -> str:
     """Returns 'android', 'ios', or the standard sys.platform name."""
-    if "ANDROID_ROOT" in os.environ or "ANDROID_ARGUMENT" in os.environ:
+    if sys.platform == "android" or "ANDROID_ROOT" in os.environ or "ANDROID_ARGUMENT" in os.environ:
         return "android"
     if sys.platform == "ios" or os.environ.get("HOME", "").startswith("/var/mobile"):
         return "ios"
